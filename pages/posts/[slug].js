1242ecc8c08c1e3440e884f0402e40e526ebdfd5
Serzhul <wilshere917@gmail.com>
2021-10-08 13:49:05 +0900

    project created

---
+++ pages/posts/[slug].js
@@ -0,0 +1,9 @@
+/* slug 형태로 구현
+slug란? 고유성을 가지는 웹사이트의 주소를 의미하며 보통 사람이 읽기 쉬운 형태로 작성되고 URL의 끝 부분에 사용된다.
+*/
+
+import React from "react";
+
+export default function PostDetailPage() {
+  return <div>테스트용</div>;
+}
