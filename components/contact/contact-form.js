370f5a9f95605368630d6021c01947b69326b1c5
Serzhul <wilshere917@gmail.com>
2021-10-10 16:23:41 +0900

    notification added .

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,29 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
 
-export default function ContactForm() {
+async function sendContact(contactData) {
   const URL = "/api/contact";
 
+  const response = await fetch(URL, {
+    method: "POST",
+    body: JSON.stringify(contactData),
+    headers: {
+      "Content-Type": "application/json", // 백엔드에서 JSON 데이터가 올 것이라고 전달해 주고 파싱이 제대로 되도록 알려줌
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+}
+
+export default function ContactForm() {
   const [inputEmail, setInputEmail] = useState("");
   const [inputName, setInputName] = useState("");
   const [inputMessage, setInputMessage] = useState("");
+  const [requestStatus, setRequestStatus] = useState(""); // pending, success, error
+  const [requestError, setRequestError] = useState("");
 
-  function sendMessageHandler(e) {
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestError(null);
+        setRequestStatus(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
+  async function sendMessageHandler(e) {
     e.preventDefault();
 
-    fetch(URL, {
-      method: "POST",
-      body: JSON.stringify({
+    setRequestStatus("pending");
+
+    try {
+      await sendContact({
         email: inputEmail,
         name: inputName,
         message: inputMessage,
-      }),
-      headers: {
-        "Content-Type": "application/json", // 백엔드에서 JSON 데이터가 올 것이라고 전달해 주고 파싱이 제대로 되도록 알려줌
-      },
-    });
+      });
+      setRequestStatus("success");
+      setInputMessage("");
+      setInputEmail("");
+      setInputName("");
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus("error");
+    }
   }
 
+  let notificationData;
+
+  if (requestStatus === "pending") {
+    notificationData = {
+      status: "pending",
+      title: "Sending message...",
+      message: "Your message is on its way",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notificationData = {
+      status: "success",
+      title: "Success!",
+
+      message: "Your message sent successfully!",
+    };
+  }
+
+  if (requestStatus === "error") {
+    notificationData = {
+      status: "error",
+      title: "Some error happened!",
+      message: requestError,
+    };
+  }
   return (
     <section className={classes.contact}>
       <h1>What can I do for you?</h1>
@@ -63,6 +124,13 @@ export default function ContactForm() {
           <button>Send message</button>
         </div>
       </form>
+      {notificationData && (
+        <Notification
+          status={notificationData.status}
+          title={notificationData.title}
+          message={notificationData.message}
+        ></Notification>
+      )}
     </section>
   );
 }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,8 @@
+import { MongoClient } from "mongodb";
+
 // Server-side 작동
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -24,7 +26,31 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://seo_abel2:6x3E2V9RPjGjc9Qi@cluster0.npqye.mongodb.net/next-blog?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Connection Failed with some reason." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db
+        .collection("contact_messages")
+        .insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
